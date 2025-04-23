d0610f6edd106d6d61ddae067da8bd536abb92a4
Open Costs <opencosts@gmail.com>
2025-04-23 23:08:42 +0530

    tmp netmind code

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
+// for the temprary  "c43e5d81612e45eca3bd501a787a38ec"
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/register', [AuthController::class, 'register']);
